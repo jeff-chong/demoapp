141f08fde9dc39c93910e9fbdcfb97536e537e54
Jeff Chong (AP) <jeff.chong@global.ntt>
2021-10-19 21:52:11 +0800

    Update the message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Small cheer and great welcome to everyone"}
+    model = {"title": "Small cheer and great welcome to webhook test"}
     return render_template('index.html', model=model)
 
 if __name__ == "__main__":
