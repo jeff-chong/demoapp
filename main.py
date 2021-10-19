0129d482797f7853bc5db6b80e56dbd7d1489672
Jeff Chong (AP) <jeff.chong@global.ntt>
2021-10-19 22:30:54 +0800

    Update message to NTT

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Small cheer and great welcome to webhook test"}
+    model = {"title": "Small cheer and great welcome to NTT"}
     return render_template('index.html', model=model)
 
 if __name__ == "__main__":
